015f8a0bcc5d4ee389fe244f3d3b3b114379e081
vannnyle@gmail.com <vannnyle@gmail.com>
2020-07-14 12:27:03 +0300

    Add dir docs

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    let myMap;
+
+// Дождёмся загрузки API и готовности DOM.
+    ymaps.ready(init);
+
+    function init() {
+        // Создание экземпляра карты и его привязка к контейнеру с
+        // заданным id ("map").
+        if (!document.getElementById('map')) {
+            return;
+        }
+        myMap = new ymaps.Map('map', {
+            // При инициализации карты обязательно нужно указать
+            // её центр и коэффициент масштабирования.
+            center: [55.76, 37.64], // Москва
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+        const btn = document.getElementById('destroyButton');
+        if (btn) {
+            btn.onclick = function () {
+                // Для уничтожения используется метод destroy.
+                myMap.destroy();
+            };
+        }
+
+    }
+
     const headerNav = document.querySelector('.header__nav');
     const menuBtn = document.querySelector('.header__toggle');
 
